c15fb89d36b00b087bc2ce553d2f5bce34504f14
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2014-02-07 10:33:25 -0800

    max_fetch_depth set

--- Streamus/Global.asax.cs
+++ Streamus/Global.asax.cs
@@ -31,7 +31,7 @@ namespace Streamus
 
         public static void InitializeApplication()
         {
-            //HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
+            HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
 
             JsonMediaTypeFormatter json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
             json.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.All;
