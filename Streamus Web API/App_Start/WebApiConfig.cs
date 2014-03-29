3275cd5fe255f5052cfe27228abdd791a7371911
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2014-03-29 10:33:49 -0700

    v.0124

--- Streamus Web API/App_Start/WebApiConfig.cs
+++ Streamus Web API/App_Start/WebApiConfig.cs
@@ -6,7 +6,7 @@ namespace Streamus_Web_API.App_Start
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
-        {
+        { 
             AutofacRegistrations.RegisterAndSetResolver(config);
         }
     }
